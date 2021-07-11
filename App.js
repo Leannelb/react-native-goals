7a30421ad8fd871fe09a47734fda7b6ec7d90b3e
leannelb <leannelaceybyrne@outlook.com>
2021-07-11 15:51:14 +0100

    triggering touchable on an item to make it fade on click

--- App.js
+++ App.js
@@ -2,46 +2,30 @@ import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
 import {
   StyleSheet,
-  Text,
-  TextInput,
   FlatList,
   View,
-  Button,
 } from "react-native";
 import GoalItem from "./components/GoalItem";
+import GoalInput from "./components/GoalInput";
 
 export default function App() {
-  const [enteredGoal, setEnteredGoal] = useState("");
   const [courseGoals, setCourseGoals] = useState([]);
 
-  const goalInputHandler = (enteredText) => {
-    setEnteredGoal(enteredText);
-  };
-
-  const addGoalHandler = () => {
-    console.log(enteredGoal);
+  const addGoalHandler = goalTitle => {
     setCourseGoals((currentGoals) => [
       ...currentGoals,
-      { id: Math.random().toString(), value: enteredGoal },
+      { id: Math.random().toString(), value: goalTitle }
     ]);
   };
 
   return (
     <View style={styles.screen}>
-      <View style={styles.inputContainer}>
-        <TextInput
-          placeholder="Course Goal"
-          style={styles.input}
-          onChangeText={goalInputHandler}
-          value={enteredGoal}
-        />
-        <Button title="ADD" onPress={addGoalHandler} />
-      </View>
+      <GoalInput onAddGoal={addGoalHandler} />
       <FlatList
         keyExtractor={(item, index) => item.id}
         data={courseGoals}
-        renderItem={(itemData) => <GoalItem title={itemData.item.value} />}
-        />
+        renderItem={(itemData) => <GoalItem onDelete={() => console.log('Does this work>?')} title={itemData.item.value} />}
+      />
     </View>
   );
 }
@@ -50,16 +34,4 @@ const styles = StyleSheet.create({
   screen: {
     padding: 50,
   },
-  inputContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-  },
-  input: {
-    width: 200,
-    borderColor: "black",
-    borderWidth: 1,
-    padding: 10,
-  },
-
 });
